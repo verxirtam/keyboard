d590b4fe77e8cfaf8707d55a9c00177903c0845c
verxirtam <d-kosuga@jcom.home.ne.jp>
2018-08-14 22:20:24 +0900

    最新のqmk_firmwareに合わせて修正

--- satan/verxirtam/keymap.c
+++ satan/verxirtam/keymap.c
@@ -1,6 +1,5 @@
 
-#include "satan.h"
-
+#include QMK_KEYBOARD_H
 
 // Used for SHIFT_ESC
 #define MODS_CTRL_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
@@ -32,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |Ctrl|Gui |Alt |      Space            |Alt |FN  |_FL |Ctrl |
    * `-----------------------------------------------------------'
    */
-[_BL] = KEYMAP_ANSI(
+[_BL] = LAYOUT_60_ansi(
   F(0),    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSPC, \
   KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSLS, \
   KC_CAPS, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,        KC_ENT , \
@@ -54,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |    |_BL |    |                        |    |    |_NL |    |
    * `-----------------------------------------------------------'
    */
-[_FL] = KEYMAP_ANSI(
+[_FL] = LAYOUT_60_ansi(
   KC_GRV ,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_6   ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,KC_DEL , \
   _______,KC_NO  ,KC_UP  ,KC_NO  ,_______,_______,RESET  ,DEBUG  ,_______,_______,KC_PSCR,KC_SLCK,KC_PAUS,_______, \
   KC_NO  ,KC_LEFT,KC_DOWN,KC_RGHT,KC_NO  ,_______,_______,_______,_______,KC_INS ,KC_HOME,KC_PGUP,        _______, \
@@ -76,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |    |_BL |    |                        |    |    |_BL |    |
    * `-----------------------------------------------------------'
    */
-[_NL] = KEYMAP_ANSI(
+[_NL] = LAYOUT_60_ansi(
   _______,KC_P1  ,KC_P2  ,KC_P3  ,KC_P4  ,KC_P5  ,KC_P6  ,KC_P7  ,KC_P8  ,KC_P9  ,KC_0   ,KC_PMNS,KC_PPLS,_______, \
   _______,_______,_______,_______,_______,_______,_______,KC_P4  ,KC_P5  ,KC_P6  ,_______,_______,_______,_______, \
   _______,_______,_______,_______,_______,_______,_______,KC_P1  ,KC_P2  ,KC_P3  ,KC_PAST,_______,        _______, \
