d17f3cfa4de8219fcd7ebb03031dc89c878cf39e
verxirtam <d-kosuga@jcom.home.ne.jp>
2018-08-24 23:28:46 +0900

    死にキーをバックスラッシュにした

--- helix/rev2/keymaps/verxirtam/keymap.c
+++ helix/rev2/keymaps/verxirtam/keymap.c
@@ -133,7 +133,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  (   |   )  |  F12 |      |      |      |PageUp|      |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |      |      |      |      |      |  ~   | XXXX |      |      |      | Home |PageDn| End  |
+   * |      |      |      |      |      |      |  ~   |  \   |      |      |      | Home |PageDn| End  |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_LOWER] = LAYOUT( \
@@ -141,7 +141,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
       _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
       _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_LPRN, KC_RPRN, KC_F12,  _______, _______, _______, KC_PGUP, _______, \
-      _______, _______, _______, _______, _______, _______, KC_TILD, XXXXXXX, _______, _______, _______, KC_HOME, KC_PGDN, KC_END \
+      _______, _______, _______, _______, _______, _______, KC_TILD, KC_BSLS, _______, _______, _______, KC_HOME, KC_PGDN, KC_END \
       ),
 
   /* Raise
@@ -154,7 +154,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * |      |  F7  |  F8  |  F9  |  F10 |  F11 |      |      |  F12 |      |      |      |PageUp|      |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |      |      |      |      |      |  `   |      |      |      |      | Home |PageDn| End  |
+   * |      |      |      |      |      |      |  `   |  |   |      |      |      | Home |PageDn| End  |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_RAISE] = LAYOUT( \
@@ -162,7 +162,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,  \
       _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
       _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______, KC_F12,  _______, _______, _______, KC_PGUP, _______, \
-      _______, _______, _______, _______, _______, _______, KC_GRV,  XXXXXXX, _______, _______, _______, KC_HOME, KC_PGDN, KC_END \
+      _______, _______, _______, _______, _______, _______, KC_GRV,  KC_PIPE, _______, _______, _______, KC_HOME, KC_PGDN, KC_END \
       ),
 
   /* Adjust (Lower + Raise)
